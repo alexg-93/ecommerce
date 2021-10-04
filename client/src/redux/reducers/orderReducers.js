ffbc16835f28ff49e635f9941b518d0659aa7c8f
Alex Gordienko <alexgordienko1993@gmail.com>
2021-10-04 09:40:39 +0300

    Update orderReducers.js
    added orderDeliverReducer

--- client/src/redux/reducers/orderReducers.js
+++ client/src/redux/reducers/orderReducers.js
@@ -22,7 +22,12 @@ import {
 
     ORDER_LIST_REQUEST,
     ORDER_LIST_SUCCESS,
-    ORDER_LIST_FAIL
+    ORDER_LIST_FAIL,
+
+    ORDER_DELIVER_REQUEST,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_FAIL,
+    ORDER_DELIVER_RESET
 
   } from "../types";
 
@@ -155,5 +160,29 @@ export const orderListReducer = (state = {orders:[]}, action) => {
       default: return state
   }
    
+}
+
+
+export const orderDeliverReducer = (state={},action) =>{
+    switch (action.type){
+        case ORDER_DELIVER_REQUEST:
+            return {
+              loading:true
+            }
+        case ORDER_DELIVER_SUCCESS:
+            return {
+                loading:false,
+                success: true
+            }
+        case ORDER_DELIVER_FAIL:
+            return {
+                loading:false,
+                error:action.payload
+            }
+        case ORDER_DELIVER_RESET:
+            return {}
+        default:
+             return state
+    }
 }
 
